19bfcca7a1101ecebb7b93480e9befc7b1bd3def
Kevin Liu <kliu25@hawk.iit.edu>
2015-11-02 07:05:33 -0600

    Added code to pull index.html and images into new webservers

--- install-env.sh
+++ install-env.sh
@@ -3,10 +3,10 @@
 sudo apt-get update -y
 sudo apt-get install -y apache2 git
 
-git clone https://github.com/Liu-Kevin/itmo-444-fall2015.git
+git clone https://github.com/Liu-Kevin/itmo-444-app.git
 
-mv ./itmo-444-fall2015/images /var/www/html/images
-mv ./itmo-444-fall2015/index.html /var/www/html
-mv ./itmo-444-fall2015/page2.html /var/www/html
+mv ./itmo-444-app/images /var/www/html/images
+mv ./itmo-444-app/index.html /var/www/html
+mv ./itmo-444-app/page2.html /var/www/html
 
 echo "Hello!" > /tmp/hello.txt
